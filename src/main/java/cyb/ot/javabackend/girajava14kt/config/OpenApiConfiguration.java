75e2c0bc813101621f865669c2f2ab28ed5d4f9e
Tuấn Nguyễn <tuannguyen2924@gmail.com>
2021-12-15 19:01:21 +0700

    fix config open api configuration
    add openapi
    add contact

--- src/main/java/cyb/ot/javabackend/girajava14kt/config/OpenApiConfiguration.java
+++ src/main/java/cyb/ot/javabackend/girajava14kt/config/OpenApiConfiguration.java
@@ -1,25 +1,32 @@
 package cyb.ot.javabackend.girajava14kt.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class OpenApiConfiguration {
+	@Value("${api.info.title}") String title;
+	
 	@Bean
 	public OpenAPI getOpenApi() {
 		return new OpenAPI()
-				.info(new Info().title("Gira Application")
-	              .description("Spring shop sample application")
-	              .version("v0.0.1")
-	              .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.info(new Info().title(title)
+	              .description("Gira application for education purpose")
+	              .version("v1.0")
+	              .license(new License().name("NO LICENSE").url("http://springdoc.org"))
+	              .contact(new Contact()
+	            		  .email("tuannguyen2924@gmail.com")
+	            		  .name("Tuan Nguyen")))
 			    .externalDocs(new ExternalDocumentation()
-	              .description("SpringShop Wiki Documentation")
-	              .url("https://springshop.wiki.github.org/docs"));
+	              .description("Spring Documentation")
+	              .url("https://spring.io/projects/spring-framework"));
 
 	}
 }
--- src/main/java/cyb/ot/javabackend/girajava14kt/role/controller/RoleController.java
+++ src/main/java/cyb/ot/javabackend/girajava14kt/role/controller/RoleController.java
@@ -3,5 +3,4 @@ package cyb.ot.javabackend.girajava14kt.role.controller;
 import cyb.ot.javabackend.girajava14kt.role.model.Role;
 
 public class RoleController {
-	
 }
